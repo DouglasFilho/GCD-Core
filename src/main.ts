9a6f5f679946f106daa93a47a3eb6fd68a077157
Douglas Rodrigues Fernandes Filho <73491081+DouglasFilho@users.noreply.github.com>
2025-11-25 00:49:44 -0300

    Adicionando dominio do front ao cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+    app.enableCors({
+    origin: [
+      'https://gcd-front.vercel.app',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
